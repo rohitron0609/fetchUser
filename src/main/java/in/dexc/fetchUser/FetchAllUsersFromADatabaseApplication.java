8b4076d9e8119e624669154d8b8b303091eb8433
rohit <rohitron0609@gmail.com>
2024-03-09 21:21:22 +0530

    Initial commit

---
+++ src/main/java/in/dexc/fetchUser/FetchAllUsersFromADatabaseApplication.java
@@ -0,0 +1,13 @@
+package in.dexc.fetchUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchAllUsersFromADatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchAllUsersFromADatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/dexc/fetchUser/FetchAllUsersFromADatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package in.dexc.fetchUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchAllUsersFromADatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
